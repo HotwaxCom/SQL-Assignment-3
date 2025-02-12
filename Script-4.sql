c2ecf54438bbb0754250eb451e4d71bfc30bd1bd
Shreya Singh <shreya.xa02@gmail.com>
2025-02-12 14:56:49 +0530

    script-4 done

--- Script-4.sql
+++ Script-4.sql
@@ -1,15 +1,14 @@
-select * from return_adjustment;
-select return_adjustment_type_id from return_adjustment where return_adjustment_type_id="appeasement";
-select * from appeasement;
-select * from return_item;
-
 select 
-	count(ri.return_item_seq_id),
-	(ri.return_quantity*ri.return_price) as return_total,
-	a.amount as appeasement_total
-from return_adjustment ra 
-join appeasement a 
-	on a.return_adjustment_type = ra.return_adjustment_type_id
-join return_item ri
-	on ri.return_id = ra.return_id;
+	count(ri.return_item_seq_id) as total_returns,
+	sum(ri.return_quantity*ri.return_price) as return_total,
+	count(ra.return_adjustment_id) as total_appeasements,
+	sum(ra.amount) as appeasement_total
+from return_header rh
+left join return_item ri
+	on rh.return_id = ri.RETURN_ID
+left join return_adjustment ra
+	on rh.return_id = ra.return_id
+	where ra.return_adjustment_type_id="APPEASEMENT";
+
+
 	
