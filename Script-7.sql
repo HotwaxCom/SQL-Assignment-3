c108fa0622121e75122168dd8cac8059c43bad81
Shreya Singh <shreya.xa02@gmail.com>
2025-02-13 11:50:09 +0530

    script-7 done

---
+++ Script-7.sql
@@ -0,0 +1,22 @@
+select
+    oisg.facility_id,
+    f.facility_name,
+    count(oisg.shipment_method_type_id) as total_one_day_shipped_orders,
+    date_format(curdate() - interval 1 month, '%Y-%m') as reporting_period
+from
+    order_item_ship_group oisg
+join 
+    facility f
+    on f.facility_id = oisg.facility_id
+where 
+    (oisg.shipment_method_type_id = 'EXPRESS' 
+     or oisg.shipment_method_type_id = 'SAME_DAY' 
+     or oisg.shipment_method_type_id = 'NEXT_DAY')
+    and oisg.created_tx_stamp >= date_format(curdate() - interval 1 month, '%Y-%m-01')
+    and oisg.created_tx_stamp < date_format(curdate(), '%Y-%m-01') 
+group by  
+    oisg.facility_id, f.facility_name
+order by 
+    total_one_day_shipped_orders desc
+limit 1; 
+
