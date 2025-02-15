0ae35a1118bddb9b347373fe2a559b206019f48f
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-02-15 21:00:51 +0530

    Update Script-12.sql

--- Script-12.sql
+++ Script-12.sql
@@ -2,7 +2,8 @@ select
 	os.order_id,
 	oh.order_date,
 	oh.status_id as order_status,
-	s.origin_facility_id as facility_id
+	s.origin_facility_id as facility_id,
+	ofc.change_datetime as duration
 from order_shipment os
 left join picklist_bin pb
 on pb.primary_order_id = os.ORDER_ID
@@ -10,7 +11,10 @@ join order_header oh
 on oh.order_id = os.order_id
 join shipment s 
 	on os.shipment_id = s.shipment_id 
+left join order_facility_change ofc 
+	on ofc.order_id = os.order_id
 	where pb.picklist_id is null
 	order by os.order_id;
+
 
 	
