45f1d5bcce705cdbc6ce90d061d17ecf91c603fe
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-07-11 10:15:47 +0530

    Update Script-10.sql

--- Script-10.sql
+++ Script-10.sql
@@ -1,3 +1,10 @@
+Question:
+
+Retailers need to study the relation of inventory levels of products to the type of facility it's stored at. Retrieve all inventory levels for products at locations and include the facility type Id. Do not retrieve facilities that are of type Virtual.
+
+Solution:
+```
+
 select 
 	ii.product_id,
 	ii.facility_id,
@@ -11,3 +18,12 @@ from inventory_item ii
 		on ft.facility_type_id = f.facility_type_id 	
 		where f.facility_type_id = "VIRTUAL_FACILITY" 
 		or ft.PARENT_TYPE_ID = "VIRTUAL_FACILITY";
+
+```
+Reasoning:
+
+Here, we needed region-specefic analysis specifically New York. So for this we took state_province_geo_id = "NY" or city ="New York" and applied joins as required in the question.
+
+```
+Query Cost: 77,126.12
+```
