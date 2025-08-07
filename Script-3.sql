afe941f47a9f3e0499c16c1189e1e55e17a87124
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-07 10:13:31 +0530

    Update Script-3.sql

--- Script-3.sql
+++ Script-3.sql
@@ -2,6 +2,10 @@ Question:
 
 The mechandising team needs a list of orders that only have one return.
 
+Fields to retrieve:
+-PARTY_ID
+-FIRST_NAME
+
 Solution:
 
 Select
@@ -11,8 +15,10 @@ from return_item ri
 join return_header rh
     on rh.return_id = ri.return_id
 join person p
+-- Getting the party information from here 
     on p.party_id = rh.from_party_id 
 group by rh.from_party_id, ri.order_id
+-- Checks if there exsits only one return to the corresponding order
 having count(ri.order_id) = 1 
 order by rh.from_party_id;
 
