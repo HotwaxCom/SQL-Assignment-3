76df027e5bff30c9b1020bb22c8f8829e11d3ac6
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-07 11:52:17 +0530

    Update Script-10.sql

--- Script-10.sql
+++ Script-10.sql
@@ -2,6 +2,13 @@ Question:
 
 Retailers need to study the relation of inventory levels of products to the type of facility it is stored at. Retrieve all inventory levels for products at locations and include the facility type Id. Do not retrieve facilities that are of type Virtual.
 
+Fields to retrieve:
+-PRODUCT_ID
+-FACILITY_ID
+-FACILITY_TYPE_ID
+-QOH (Quantity on Hand)
+-ATP (Available to Promise)
+
 Solution:
 	
 SELECT
@@ -13,13 +20,7 @@ SELECT
 from inventory_item ii
 	join facility f
 		on f.facility_id = ii.facility_id
-	join facility_type ft
-		on ft.facility_type_id = f.facility_type_id 	
-		where f.facility_type_id = "VIRTUAL_FACILITY" 
-                or ft.PARENT_TYPE_ID = "VIRTUAL_FACILITY";
-	
-Reasoning:
-Here, we needed region-specefic analysis specifically New York. So for this we took state_province_geo_id = "NY" or city ="New York" and applied joins as required in the question.
+where f.facility_type_id != "VIRTUAL_FACILITY";
 
-Query Cost: 1,631,251.28
+Query Cost: 908,816.97
 
