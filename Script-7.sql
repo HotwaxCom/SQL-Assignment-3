2884964279a7078a3a1cce77c94e71da84220b4a
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-07 11:00:17 +0530

    Update Script-7.sql

--- Script-7.sql
+++ Script-7.sql
@@ -1,30 +1,36 @@
 Question:
 
-Companies often want region-specific analysis to plan local marketing, staffing, or promotions in certain areas—here, specifically, New York.
+Identify which facility (store) handled the highest volume of “one-day shipping” orders in the previous month, useful for operational benchmarking.
+
+Fields to retrieve:
+-FACILITY_ID
+-FACILITY_NAME
+-TOTAL_ONE_DAY_SHIP_ORDERS
+-REPORTING_PERIOD
 
 Solution:
 
 select
     oisg.facility_id,
     f.facility_name,
-    count(oisg.shipment_method_type_id) as total_one_day_shipped_orders,
+    count(distinct(oisg.order_id)) as total_one_day_shipped_orders,
     date_format(curdate() - interval 1 month, '%Y-%m') as reporting_period
 from
     order_item_ship_group oisg
+join item_issuance ii
+	on ii.order_id = oisg.order_id
 join 
     facility f
     on f.facility_id = oisg.facility_id
 where 
-    (oisg.shipment_method_type_id = 'EXPRESS' 
-     or oisg.shipment_method_type_id = 'SAME_DAY' 
-     or oisg.shipment_method_type_id = 'NEXT_DAY')
-    and oisg.created_tx_stamp >= date_format(curdate() - interval 1 month, '%Y-%m-01')
-    and oisg.created_tx_stamp < date_format(curdate(), '%Y-%m-01') 
+    oisg.shipment_method_type_id IN ('EXPRESS','SAME_DAY','NEXT_DAY')
+    and ii.issued_date_time >= DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-01')
+    and ii.issued_date_time <  DATE_FORMAT(CURDATE(), '%Y-%m-01')
 group by  
     oisg.facility_id, f.facility_name
 order by 
     total_one_day_shipped_orders desc
-limit 1; 
+limit 1;
 
-Query Cost: 1.76
+Query Cost: 15,440.34
 
