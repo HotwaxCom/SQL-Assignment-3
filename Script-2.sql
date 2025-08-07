ddb78f5f6ef9d7ac679ee0408ad76847973b1e64
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-07 10:10:41 +0530

    Update Script-2.sql

--- Script-2.sql
+++ Script-2.sql
@@ -23,17 +23,16 @@ select
 	rs.status_datetime,
 	oh.order_name,
 	rh.from_party_id,
-	rh.entry_date as return_date,
+	rh.return_date,
 	rh.return_channel_enum_id
-from return_item ri
-join return_header rh
-	on ri.return_id=rh.RETURN_ID
+from return_header rh
+join return_item ri
+	on ri.return_id=rh.return_id 
 join return_status rs
-	on rs.return_id=rh.return_id
+	on rs.return_id=rh.return_id and rs.status_id = rh.status_id
 join order_header oh
 	on oh.order_id=ri.order_id
-	where rh.status_id=rs.status_id
-	and ri.return_item_seq_id=rs.return_item_seq_id
+	where ri.return_item_seq_id=rs.return_item_seq_id
 	order by return_id;
 
-Query Cost: 15,327.48
+Query Cost: 15,874.35
